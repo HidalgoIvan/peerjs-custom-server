354b67664644b4edba6a35d684bfadd86c856877
Iván Hidalgo <iochoa@ndscognitivelabs.com>
2020-05-25 22:33:11 -0500

    why is node like this

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+	var cmd = require("node-cmd");
+
+	cmd.get("peerjs --port 9000 --key peerjs --path /server", function (
+		err,
+		data,
+		stderr
+	) {
+		console.log("result: ", data);
+		console.log("err:", err);
+		console.log("stderr:", stderr);
+	});
+
 	return <div className="App">Hi! You shouldn't be seeing this!</div>;
 }
 
