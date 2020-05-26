245b02116f0196b35d28fb13dc1feff46cf1f0e1
Iván Hidalgo <iochoa@ndscognitivelabs.com>
2020-05-25 22:28:56 -0500

    peerjs is stupid

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const { PeerServer } = require("peer");
-
-const peerServer = PeerServer({ port: 9000, path: "/server" });
 function App() {
 	return <div className="App">Hi! You shouldn't be seeing this!</div>;
 }
