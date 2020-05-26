61d757aaff47e91be8025a05210876008f57320d
Iván Hidalgo <iochoa@ndscognitivelabs.com>
2020-05-25 22:35:51 -0500

    react is stupid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+var cmd = require("node-cmd");
 
 function App() {
-	var cmd = require("node-cmd");
-
 	cmd.get("peerjs --port 9000 --key peerjs --path /server", function (
 		err,
 		data,
