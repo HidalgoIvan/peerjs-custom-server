ec9cfa016eb9e10b57aa605eb905a7c63a8afdea
Iván Hidalgo <iochoa@ndscognitivelabs.com>
2020-05-25 22:18:04 -0500

    servers are stupid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const { PeerServer } = require("peer");
+
+const peerServer = PeerServer({ port: 9000, path: "/server" });
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className="App">Hi! You shouldn't be seeing this!</div>;
 }
 
 export default App;
