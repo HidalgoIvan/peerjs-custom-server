e48fd4236a351c33629b5bb1c777fa1f8e7c01e6
Iván Hidalgo <iochoa@ndscognitivelabs.com>
2020-05-25 22:05:13 -0500

    Updates

---
+++ precache-manifest.2784dabafd34cda84a745145669fb5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912267c91f6dcd2c37fced3b6177887e",
+    "url": "/peerjs-custom-server/index.html"
+  },
+  {
+    "revision": "ad491561dc5cdecf807a",
+    "url": "/peerjs-custom-server/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6378353648a54fe52d2a",
+    "url": "/peerjs-custom-server/static/js/2.5d6472b4.chunk.js"
+  },
+  {
+    "revision": "aeacff1fc9d7c4606fe6c6435329b18f",
+    "url": "/peerjs-custom-server/static/js/2.5d6472b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad491561dc5cdecf807a",
+    "url": "/peerjs-custom-server/static/js/main.72b01435.chunk.js"
+  },
+  {
+    "revision": "c113202179e69983dbe6",
+    "url": "/peerjs-custom-server/static/js/runtime-main.77f86a30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/peerjs-custom-server/static/media/logo.5d5d9eef.svg"
+  }
+]);
